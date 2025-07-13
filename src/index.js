03b005f2f4c4f6037cfa89dec4144ce4ce0da5bf
Rushikesh <rushikesh.jagtap@uopeople.edu>
2025-07-13 16:06:54 +0530

    check

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ Coded by www.creative-tim.com
 
 import React from "react";
 import * as ReactDOMClient from "react-dom/client";
-import { HashRouter } from 'react-router-dom';
+import { HashRouter } from "react-router-dom";
 import App from "App";
 
 const container = document.getElementById("root");
