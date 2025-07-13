d3bce47f051c127803edd498a64cec85bf5f590a
Rushikesh <rushikesh.jagtap@uopeople.edu>
2025-07-13 14:14:54 +0530

    app js

--- src/App.js
+++ src/App.js
@@ -56,8 +56,8 @@ export default function App() {
       <CssBaseline />
       <Routes>
         {getRoutes(routes)}
-        <Route path="/presentation" element={<Presentation />} />
-        <Route path="*" element={<Navigate to="/presentation" />} />
+        <Route path="/locus" element={<Presentation />} />
+        <Route path="*" element={<Navigate to="/locus" />} />
       </Routes>
     </ThemeProvider>
   );
