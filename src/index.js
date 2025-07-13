347da5d400effca12ea0dcf3342afe0332569794
Rushikesh <rushikesh.jagtap@uopeople.edu>
2025-07-13 12:22:05 +0530

    BrowserRouter

--- src/index.js
+++ src/index.js
@@ -24,7 +24,7 @@ const container = document.getElementById("root");
 const root = ReactDOMClient.createRoot(container);
 
 root.render(
-  <BrowserRouter basename="/locus">
+  <BrowserRouter>
     <App />
   </BrowserRouter>
 );
